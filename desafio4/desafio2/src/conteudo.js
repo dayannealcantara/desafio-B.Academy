f54b7b581d175faf272d2cc94af517bf6362aa17
Dayanne Alcantara <dayannealcantara14@gmail.com>
2021-09-08 12:11:05 -0300

    estilização conteúdo (desafio2) - desafio4

--- desafio4/desafio2/src/conteudo.js
+++ desafio4/desafio2/src/conteudo.js
@@ -1,36 +1,65 @@
 import Title from "./titulo";
+import styled from "styled-components";
 import lavanda from './imagens/lavanda.jpg'
 import hortela from './imagens/hortela.jpg'
 import alecrim from './imagens/alecrim.jpg'
 
 function Conteudo({ title, content}) {
   return (
-    <div className="conteudo">
+    <Div>
            
       <Title size="2"> Um pouco sobre os óleos essenciais</Title>
-      <p>
+      <P>
       Os óleos essenciais são compostos voláteis produzidos pelas plantas para a sua sobrevivência, pois são substâncias químicas que exercem as funções de defesa e de atração de polinizadores, além de inibir a ação de alguns herbívoros. Alguns protegem contra o ataque de bactérias ou fungos, outros são conhecidos por serem alelopáticos. Os componentes majoritários são os sesquiterpenóides e monoterpenóides, os quais são altamente voláteis e contribuem para a fragrância ou essência das plantas que os produzem.
-      </p>
+      </P>
 
       <Title size="2"> Alguns Benefícios</Title>
-      <p>No corpo, por apresentarem funções terapêuticas, farmacológicas e cosméticas, os óleos essenciais têm como principais benefícios:</p>
+      <P>No corpo, por apresentarem funções terapêuticas, farmacológicas e cosméticas, os óleos essenciais têm como principais benefícios:</P>
       <ul> 
-        <li className="beneficios">Melhora e promoção da sensação de bem-estar;</li>
-        <li className="beneficios">Alívio nos sintomas de doenças;</li>
-        <li className="beneficios">Amenização de dores;</li>
-        <li className="beneficios">Fortalecimento das defesas do corpo;</li>
-        <li className="beneficios">Opção natural na composição de cosméticos.</li>
+        <Li>Melhora e promoção da sensação de bem-estar;</Li>
+        <Li>Alívio nos sintomas de doenças;</Li>
+        <Li>Amenização de dores;</Li>
+        <Li>Fortalecimento das defesas do corpo;</Li>
+        <Li>Opção natural na composição de cosméticos.</Li>
       </ul>
-      <img src={lavanda} alt='óleo lavanda' />
-      <img src={hortela} alt='óleo hortela' />
-      <img src={alecrim} alt='óleo alecrim' />
+
+      <Img src={lavanda} alt='óleo lavanda' />
+      <Img src={hortela} alt='óleo hortela' />
+      <Img src={alecrim} alt='óleo alecrim' />
+
       <Title size="3">{title}</Title>
          
-      <Title size="4">{content}</Title>   
+      <Title size="4">{content}</Title>  
+       
 
-     </div>
+     </Div>
     
   );
 }
+
+const Div = styled.div`
+background-color: #F9E6E6;
+width: 80%;
+text-align: center;
+padding: 20px;
+`;
+
+const P = styled.p`
+text-align: justify;
+padding: 0 20px;
+line-height: 25px;
+`;
+
+const Li = styled.li`
+text-align: justify;
+line-height: 25px;
+`;
+
+const Img = styled.img`
+width: 200px;
+padding: 20px;
+
+`;
+
 
 export default Conteudo;
--- desafio4/desafio2/src/sidebar.js
+++ desafio4/desafio2/src/sidebar.js
@@ -1,27 +1,43 @@
+import styled from "styled-components";
 
+function Sidebar({ links, setTitle, setContent }) {
+  function handleClick(e, link) {
+    e.preventDefault();
+    setTitle(link.title);
+    setContent(link.content);
+  }
 
-function Sidebar({links, setTitle, setContent}) {
-
-   function handleClick(e,link){
-       e.preventDefault()
-     setTitle(link.title)
-     setContent(link.content)
-       
-   }
-
-    return (
-        
-        <div className='sidebar'>  
-            <ul className='links-sidebar'>
-                {links.map((link) => (
-                <li key= {link.id} >
-                    <a  onClick={ (e) =>handleClick(e, link)} href={link.title}>{link.title}</a>
-                </li> 
-              ))}
-            </ul>
-           
-        </div>
-    )
+  return (
+    <Div>
+      <ul>
+        {links.map((link) => (
+          <Li key={link.id}>
+            <A onClick={(e) => handleClick(e, link)} href={link.title}>
+              {link.title}
+            </A>
+          </Li>
+        ))}
+      </ul>
+    </Div>
+  );
 }
 
-export default Sidebar
+const Div = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 20%;
+  background-color: #d8abb1;
+`;
+
+const Li = styled.li`
+  padding-top: 100px;
+`;
+const A = styled.a`
+  color: black;
+  text-align: justify;
+  text-decoration: none;
+  font-size: 16px;
+  line-height: 20px;
+`;
+
+export default Sidebar;
