983d38991220933e7dca650af5b302d58d0bbef2
Dayanne Alcantara <dayannealcantara14@gmail.com>
2021-09-16 11:16:07 -0300

    iniciando desafio 6

---
+++ desafio6/src/@types/styled.d.ts
@@ -0,0 +1,13 @@
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    colors: {
+      black: string
+      lightBlack: string
+      gray: string
+      White: string
+      primary: string
+      primaryDark: string
+    }
+  }
+}
+export {}
