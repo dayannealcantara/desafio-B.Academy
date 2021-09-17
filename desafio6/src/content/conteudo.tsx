55d669414897d208c9979c7b12d90bbe31da57e2
Dayanne Alcantara <dayannealcantara14@gmail.com>
2021-09-17 09:04:14 -0300

    configurando style

--- desafio6/src/content/conteudo.tsx
+++ desafio6/src/content/conteudo.tsx
@@ -1,5 +1,20 @@
 import { useState, ChangeEvent } from 'react'
 import * as S from './content-style'
+import marked from 'marked'
+
+import 'highlight.js/styles.github.css'
+
+import('highlight.js').then(hljs => {
+  const h = hljs.default
+  marked.setOptions({
+    highlight: (code, language) => {
+      if (language && h.getLanguage(language)) {
+        return h.highlight(code, { language }).value
+      }
+      return h.highlightAuto(code).value
+    },
+  })
+})
 
 function Content () {
   const [content, setContent] = useState(' ')
@@ -20,9 +35,8 @@ function Content () {
           onChange={handleChange}
         />
 
-        <S.Article>
-          {content}
-        </S.Article>
+        <S.Article dangerouslySetInnerHTML={{ __html: marked(content) }} />
+
       </S.ContentSection>
     </S.ContentWrapper>
   )
